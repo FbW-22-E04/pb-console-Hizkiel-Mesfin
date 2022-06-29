000a11aee548de6ce2f8d151508b72fa2bcda954
Hizkiel-Mesfin <mesfinezekiel@gmail.com>
2022-06-29 12:13:17 +0200

    console dot logged everything

--- solution.js
+++ solution.js
@@ -1,1 +1,27 @@
 // write your code in this file
+
+//  task #1
+console.log("This is an exercise in console logging");
+
+// task #2
+const exercise = "This is the value.";
+console.log(exercise);
+
+// task #3
+const firstName = "Hizkiel";
+const lastNAme = "Mesfin";
+
+console.log(firstName, lastNAme);
+
+// task #4
+const age = 29;
+const city = "Neumünster";
+const job = "student";
+
+console.log(firstName, lastNAme, age, city, job);
+
+// task #5
+console.log(
+  firstName,
+  lastNAme + " is a " + age + " years old " + job + " who lives in " + city
+);
